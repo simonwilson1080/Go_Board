e06b291fc6a3e5cf91348a24e3a80c5f799f0d44
Simon Wilson <simonwilson1080@gmail.com>
2026-02-07 14:50:26 -0700

    added gui logic and tested on a separate board array. will implement into other board later.

--- src/App.java
+++ src/App.java
@@ -5,12 +5,27 @@
  * A simple GO Board
 *********************************************************/
 
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Scanner;
 
 public class App {
 
     static String[][] board = new String[9][9];
+    static String[][] gui_board = {
+        {null, null, null, null, null, null, null, null, null},
+        {null, null, "O", "O", "O", null, null, null, null},
+        {null, null, "O", "@", "O", null, null, null, null},
+        {null, null, "O", "@", "O", null, null, null, null},
+        {null, null, null, "O", null, "@", null, null, null},
+        {null, null, null, null, null, "@", null, null, null},
+        {null, null, null, null, null, "@", null, null, null},
+        {null, null, null, null, null, "@", null, null, null},
+        {null, null, null, null, null, null, null, null, null},
+    };
+    static int GAP = 30;
+    static int MARGIN = 10;
 
     static void printGoBoard(String[][] board) {
         for(int i = 0; i < board.length; i++){
@@ -34,7 +49,44 @@ public class App {
         }
     }
 
+    static void showUI(String[][] args) throws Exception {
+
+        JFrame frame = new JFrame("Go");
+        frame.setSize(GAP * gui_board.length, (GAP+2) * gui_board.length);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.getContentPane().setBackground("green");
+
+        frame.add(new Component() {
+            public void paint(Graphics g) {
+
+                //Board
+                g.setColor(Color.BLACK);
+                for (int i = 0; i < gui_board.length; i++) {
+                    g.drawLine(MARGIN, MARGIN + i * GAP, MARGIN + (gui_board.length-1) * GAP, MARGIN + i * GAP);
+                    g.drawLine(MARGIN + i * GAP, MARGIN, MARGIN + i * GAP, MARGIN + (gui_board.length-1) * GAP);
+                }
+
+                //Stones
+                for (int x = 0; x < gui_board.length; x++) {
+                    for (int y = 0; y < gui_board.length; y++) {
+                        if (gui_board[x][y] != null) {
+                            g.setColor(gui_board[x][y] == "@" ? Color.BLACK : Color.WHITE);
+                            g.fillOval(MARGIN + x * GAP - 12, MARGIN + y * GAP - 12, 24, 24);
+                            g.setColor(Color.BLACK);
+                            g.drawOval(MARGIN + x * GAP - 12, MARGIN + y * GAP - 12, 24, 24);
+                        }
+                    }
+                }
+            }
+        });
+
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) throws Exception {
+
+        showUI(gui_board);
+
         boolean playing = true;
         boolean turn = true;
         Scanner scn = new Scanner(System.in);
@@ -55,7 +107,7 @@ public class App {
             String temp = board[y-1][x-1];
             //ⵔ⬤⚪⚫
 
-            if(turn == true){
+            if(turn == true) {
                 switch (temp) { 
                 case "+":
                     board[y-1][x-1] = "o";
